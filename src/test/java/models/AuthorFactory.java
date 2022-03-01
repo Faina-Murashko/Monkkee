ef8408c453540d093ad415d1135ca185bcf58f90
Faina-Murashko <fainka18@gmail.com>
2022-03-01 18:20:18 +0300

    changes new

--- src/test/java/models/AuthorFactory.java
+++ src/test/java/models/AuthorFactory.java
@@ -7,7 +7,7 @@ import lombok.extern.log4j.Log4j2;
 public class AuthorFactory {
     static Faker faker = new Faker();
     public static Author get(){
-        log.info("Building java faker to randomly enter name.");
+        log.info("Building java faker to randomly enter name, email, text for comment.");
         return Author.builder()
                 .name(faker.name().fullName())
                 .comment(faker.lebowski().character())
